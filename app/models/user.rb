4483bec9254097c5d4b8d92a1fd4f1a8fc1428c2
Kevin Malta <kevin.malta@gmail.com>
2015-01-24 17:25:23 -0800

    adds trevors sample app to our website

---
+++ app/models/user.rb
@@ -0,0 +1,63 @@
+class User < ActiveRecord::Base
+	attr_accessor :remember_token, :activation_token	
+	before_save   :downcase_email
+    before_create :create_activation_digest 
+
+	validates :name,  presence: true, length: { maximum: 50 }
+	validates :email, presence: true, length: { maximum: 255 },
+			format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+			uniqueness: { case_sensitive: false }
+
+	has_secure_password
+	validates :password, length: { minimum: 6 }, allow_blank: true
+
+	# Returns the hash digest of the given string.
+	def User.digest(string)
+	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+	                                              BCrypt::Engine.cost
+	BCrypt::Password.create(string, cost: cost)
+	end
+
+	# Returns a random token.
+	def User.new_token
+	SecureRandom.urlsafe_base64
+	end
+	# Remembers a user in the database for use in persistent sessions.
+	def remember
+	self.remember_token = User.new_token
+	update_attribute(:remember_digest, User.digest(remember_token))
+	end
+	# Returns true if the given token matches the digest.
+	  def authenticated?(attribute, token)
+	    digest = send("#{attribute}_digest")
+	    return false if digest.nil?
+	    BCrypt::Password.new(digest).is_password?(token)
+	  end
+
+	# Forgets a user.
+	def forget
+	update_attribute(:remember_digest, nil)
+	end
+	def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+ 	end
+	# Sends activation email.
+	def send_activation_email
+	UserMailer.account_activation(self).deliver_now
+	end
+
+	 private
+
+    # Converts email to all lower-case.
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+
+end
