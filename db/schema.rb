e15812481efc8eec57d0a6e41de19d9145c2d76f
Trevor <trevor@ubuntu.(none)>
2015-01-25 22:37:06 -0800

    added emails to user model and added a migration for the db model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125204046) do
+ActiveRecord::Schema.define(version: 20150126063252) do
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150125204046) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "email"
   end
 
 end
