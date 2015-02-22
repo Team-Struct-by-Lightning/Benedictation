8cc13f5b519582f52cc67fbd0bb6300010bc351b
Kevin Malta <kevin.malta@gmail.com>
2015-02-22 12:48:44 -0800

    adds refresh tokens to database

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,6 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
   '165043768486-h54df8d1s3id58p9c603q3cq90pe60it.apps.googleusercontent.com',
-  'fuXwRcqNZAu7iBIqwzAliLx_', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  'fuXwRcqNZAu7iBIqwzAliLx_', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}},
+  access_type: 'offline',  prompt: 'consent' }
 end
