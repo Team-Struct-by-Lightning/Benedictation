f82c44ba43fa859e89e837b855f74530e949fd25
Trevor <trevor@ubuntu.(none)>
2015-02-23 14:56:10 -0800

    moved current_user to sessions_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,5 +26,8 @@ module SessionsHelper
     session[:forwarding_url] = request.url if request.get?
   end
 
+def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
 end
