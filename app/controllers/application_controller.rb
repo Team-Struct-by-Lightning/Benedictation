a1f238db1654eb98b42aa42efc5040893df0301b
Kevin Malta <kevin.malta@gmail.com>
2015-02-22 23:40:20 -0800

    adds hover over for groups in chat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
+  include ApplicationHelper
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  
+
 end
