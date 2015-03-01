eac5f6f87688c52f40bde2061c06bfb4192d7d1d
Kevin Malta <kevin.malta@gmail.com>
2015-02-28 17:50:14 -0800

    flashes danger for invalid group creation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,9 +19,11 @@ class GroupsController < ApplicationController
         flash[:success] = "Added the group " + @group.group_name
         redirect_to chat_path
       else
+        flash.now[:danger] = 'Invalid group name: Valid groups contain 1-15 valid characters'
         render 'new'
       end
     else
+      flash.now[:danger] = 'Invalid group name: Valid groups contain 1-15 valid characters'
       render 'new'
     end
   end
