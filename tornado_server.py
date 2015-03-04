0e2e516d19506c8b6cf340085a415e2a5e172975
Britt Christy <iamburitto@gmail.com>
2015-03-03 16:17:50 -0800

    added evan's updated tornado_server.py code for reals this time

--- tornado_server.py
+++ tornado_server.py
@@ -3,18 +3,14 @@ import tornado.web
 import tornado.websocket
 import tornado.httpserver
 from nltk_test import find_nouns
-#class MainHandler(tornado.web.RequestHandler):
-#	def get(self):
-#		print 'yo im a get request my niggah'
-#		self.write("Hello, world")
-
-#hey = find_nouns("this is the sentence")
-#print hey
+import speechrec    # Put speechrec.py in the same folder
+import wave
+import os
 
 class WSHandler(tornado.websocket.WebSocketHandler):
 	def open(self):
 		print 'new connection'
-		self.write_message("Hello Evan")
+		self.write_message("Hello from the Python Server!")
 
 	def on_message(self, message):
 		print 'message received %s' % message
@@ -29,13 +25,44 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 		#print parsed_origin
 		return True
 
+# Handle audio data sent to /recognize.
+class SpeechWSHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+		print "New connection to speech recognizer opened"
+        self.recognizer = SpeechRecognizer()
+        self.recording = False
+        
+    def on_message(self, message):
+        print "speech-rec received message: %s" % message
+        if self.recording == False and message == "start":
+                self.recording = True
+        if self.recording == True:
+            if message == "stop":
+                self.recording = False
+                self.write_message("test response %s" % message)
+            else:
+                # message is a wav file, fully encoded, *with headers*.
+                # is it a file-like object? can Python read it? or is it a raw binary array which I must index into past the headers and use?
+                #msg_wav = wave.open(message, "rb")
+                print type(message)
+                message_data = str(message)[44:]    # this skips the 44-byte header and gets the data
+                # write this to an open wavfile object
+                
+        
+    def on_close(self):
+        print "Connection closed."
+        os.remove("speech.wav")
+    
+    def check_origin(self):
+        return True
+    
+    
 application = tornado.web.Application([
 	(r"/hello", WSHandler),
+    (r"/recognize", SpeechWSHandler)
 ])
 
 if __name__ == "__main__":
-	#application.listen(8888)
-	#tornado.ioloop.IOLoop.instance().start()
 	http_server = tornado.httpserver.HTTPServer(application)
 	http_server.listen(8888)
 	tornado.ioloop.IOLoop.instance().start()
