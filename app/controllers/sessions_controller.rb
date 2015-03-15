4a8ead96be7dd7115882a38d45841f2bb949908e
Kevin Malta <kevin.malta@gmail.com>
2015-03-14 23:32:43 -0700

    puts back in csrf protection

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
 
-#this is really weird - why do i need to include this file? it was always auto-included in the sample app
 include SessionsHelper
 
   def create
     created_user = User.from_omniauth(env["omniauth.auth"])
+    create_state
     log_in created_user
     redirect_back_or chat_path
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,9 +6,14 @@ module SessionsHelper
 
 	def log_in(user)
     	session[:user_id] = user.id
-  	end
+  end
 
-  	def log_out
+  def create_state
+    state = get_random_string
+    session[:state] = state
+  end
+
+  def log_out
 		session.delete(:user_id)
 		@current_user = nil
 	end
