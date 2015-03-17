af621ca45a6563f0d75a160f5fdd4cc4b8180638
Kevin Malta <kevin.malta@gmail.com>
2015-03-16 20:43:34 -0700

    deletes groups with flash success

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,11 +2,15 @@ class RelationshipsController < ApplicationController
 
 	def destroy
 		groupID = Relationship.find_by_id(params[:id]).group_id
+		group_name = Group.find_by_id(groupID).group_name
 
 		Relationship.delete(Relationship.find_by_id(params[:id]).group_id)
 
 	  if (Relationship.where(group_id: groupID).empty?)
 	  	Group.delete(Group.find_by_id(groupID))
+	  	flash[:success] = "You have successfully deleted the group: #{group_name}."
+		else
+			flash[:success] = 'You have successfully left the group: #{group_name}.'
 	  end
 	  redirect_to chat_path
 	end
