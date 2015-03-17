f60c53492f27c758391fd7cd7557b679ee3883c2
Kevin Malta <kevin.malta@gmail.com>
2015-03-16 18:22:31 -0700

    corrects error message if group does not exist in database

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -13,7 +13,10 @@ module RoomsHelper
 		end
 
 		if url_array[url_index].to_i.to_s == url_array[url_index]
-			if Relationship.where(group_id: url_array[url_index].to_i, user_id: current_user.id).empty?
+			if Group.where(id: url_array[url_index].to_i, group_name: url_array[url_index + 1]).empty?
+				flash[:danger] = 'This group does not exist. Create a group in the sidebar to the left and join its room.'
+				redirect_to chat_path
+			elsif Relationship.where(group_id: url_array[url_index].to_i, user_id: current_user.id).empty?
 				flash[:danger] = 'You are not a member of this group. You must be invited to the group in order to join this room.'
 				redirect_to chat_path
 			end
