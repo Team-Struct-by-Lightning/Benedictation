fc6c93a477a9b2f645b5346dede40e58c8a71fec
Kevin Malta <kevin.malta@gmail.com>
2015-03-18 10:26:34 -0700

    adds proper redirects to newuser page

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,16 +2,11 @@ module GroupsHelper
 	def get_group_id
 		url_string = request.original_url
 		url_array = url_string.split('/')
-		url_index = 0;
+		url_array[-1]
+	end
 
-		while url_index < url_array.size
-			if url_array[url_index] == 'newuser'
-				url_index += 1
-				break
-			end
-		url_index += 1
-		end
-
-		url_array[url_index]
+	def get_current_group_name
+		id = get_group_id
+		Group.find(id).group_name
 	end
 end
