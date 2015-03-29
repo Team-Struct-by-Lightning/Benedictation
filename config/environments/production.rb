480675c4352af899c898ba02a387ad5470c2e9c1
Britt Christy <iamburitto@gmail.com>
2015-03-29 13:57:39 -0700

    changed production.rb to have secret key base for the production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
