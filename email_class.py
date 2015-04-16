a8fcb8ea1808cd254964cff979d1cc133d9b5a8d
Kevin Malta <kevin.malta@gmail.com>
2015-04-15 18:11:01 -0700

    email code latest version to send to Britt for debug

--- email_class.py
+++ email_class.py
@@ -19,30 +19,40 @@ f = open('passwords.yml')
 yml_dict_email = yaml.safe_load(f)
 f.close()
 
-class email_sender:
+class email_sender():
 
   team_email = "team.struct.by.lightning@gmail.com"
 
-  def _init_(sender_name, sender_email, group_join, recipient_email):
+  def __init__(self, sender_name, sender_email, group_join, recipient_email, email_type):
     self.sender_name = sender_name
     self.sender_email = sender_email
     self.recipient_email = recipient_email
     self.group_join = group_join
+    self.email_type = email_type
 
-  def personalize_message(send_name, send_email, join_group, recip_email, email_type):
+  def personalize_message(self, send_name, send_email, join_group, recip_email, email_type):
 
     if email_type == "invite":
-      return "Hello!" + os.linesep + os.linesep + "You have been invited into the Benedictation system.  Benedictation is a video conferencing web application with a smart assistant for your meeting needs.  " + send_name + " invited you to join the chat group " + join_group + ".  Follow this link to join the group:" + os.linesep + os.linesep + "https://benedictation.io" + os.linesep + os.linesep + "Thank you and enjoy!" + os.linesep + os.linesep + "Team Struct by Lightning"
+      ret_string = ("Hello! \n \n You have been invited into the"
+                    "Benedictation system.  Benedictation is a video conferencing web application"
+                    "with a smart assistant for your meeting needs.  " + send_name + " invited "
+                    "you to join the chat group " + join_group + ".  Follow this link to join the "
+                    "group: \n\n https://benedictation.io \n\n"
+                    "Thank you and enjoy! \n\n Team Struct"
+                    " by Lightning")
+      return ret_string
+
+    return ""
 
 
-  def send_email(send_name, send_email, join_group, recip_email, email_type):
+  def send_email(self, send_name, send_email, join_group, recip_email, email_type):
     # Open a plain text file for reading.  For this example, assume that
     # the text file contains only ASCII characters.
 
     msg = MIMEMultipart()
 
     # Create a text/plain message
-    text = MIMEText(personalize_message(send_name, send_email, join_group, recip_email, email_type))
+    text = MIMEText(self.personalize_message(send_name, send_email, join_group, recip_email, email_type))
     msg.attach(text)
 
     msg['Subject'] = send_name + ' invited you to join Benedictation'
@@ -53,8 +63,13 @@ class email_sender:
     server.starttls() # upgrades plain text to secure
 
     # log in to the server
-    server.login("team.struct.by.lightning", yml_dict_email['email']['passwords'])
+    server.login("team.struct.by.lightning", str(yml_dict_email['email']['password']))
 
     # Send the message via our own SMTP server
     server.sendmail(send_email, [recip_email], msg.as_string())
     server.quit()
+
+if __name__ == "__main__":
+  se = email_sender("Kevin Malta", "team.struct.by.lightning@gmail.com", "Scrum", "iamburitto@gmail.com", "invite")
+  se.send_email(se.sender_name, se.sender_email, se.recipient_email, se.group_join, se.email_type)
+
