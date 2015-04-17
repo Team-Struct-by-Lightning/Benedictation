f4601da9a922e08fd97a78c281b5ec004e480cc7
Britt Christy <iamburitto@gmail.com>
2015-04-17 12:58:46 -0700

    Sends emails to new users and adds them to groups when they log in for the first time.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,10 @@ class SessionsController < ApplicationController
     new_group_list = $redis.lrange(created_user.email,0,-1)
     # for each new group, add a new relationship
     new_group_list.each do |group|
-        @new_group_relationship = Relationship.new(user_id:created_user.id, group_id:Group.find_by_id(group).id)
-        @new_group_relationship.save
+        unless Group.where(id: group).empty?
+            @new_group_relationship = Relationship.new(user_id:created_user.id, group_id:Group.find_by_id(group).id)
+            @new_group_relationship.save
+        end
     end
     # clear group list for this email
     $redis.del(created_user.email)
