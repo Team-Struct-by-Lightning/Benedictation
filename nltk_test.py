f9e0dac162ed91bb6abbc833c745984c03324c6a
Trevor <trevor.frese@gmail.com>
2015-04-17 18:32:36 -0700

    got 8/9 test cases to pass

--- nltk_test.py
+++ nltk_test.py
@@ -37,19 +37,33 @@ def schedule_meeting(sentence):
 	tree = parser.parse(sentence)
 	cal = parsedatetime.Calendar()
 
-	#print cal.parse("tomorrow")
+	#print cal.parse("tomorrow 4 pm ")
+	#print cal.parse("tomozzzz buhhh ")
 	
-	print tree
+	#print tree
 
 	for element in [tree] + [e for e in tree]: #needed to include root
 		if 'VP' in element.label():
 			for verb_subtrees in element.subtrees():
 				if 'VB' in verb_subtrees.label() \
 				and any(x in verb_subtrees.leaves() for x in schedule_verbs):
-
-					for noun_subtrees in element.subtrees():
-						if noun_subtrees.label() == 'NP' and any(x in noun_subtrees.leaves() for x in schedule_nouns):
+					for VP_subtrees in element.subtrees():
+						if VP_subtrees.label() == 'NP' and any(x in VP_subtrees.leaves() for x in schedule_nouns):
 							print "True"
+
+						#print VP_subtrees.leaves()
+						if VP_subtrees.label() == 'PP':
+							words = ' '.join(VP_subtrees.leaves())
+							if cal.parse(words)[1] != 0:
+								print "True"
+
+
+						
+							#print cal.parse(VP_subtrees.leaves()) 
+						#if VP_subtrees.label() == 'PP' and cal.parse(): 
+
+
+
 
 
 
@@ -71,7 +85,34 @@ def schedule_meeting(sentence):
 #def tree_parser(tree):
 #	if 'VP' in tree.label():
 
+print "Test 1"
+schedule_meeting("Let's schedule a meeting for tomorrow")
+print 
+print "test 2"
+schedule_meeting("Let's meet up at 4 PM tomorrow")
+print 
+print "test 3"
+schedule_meeting("Can we meet Thursday at noon")
+print 
+print "test 4"
+schedule_meeting("Can we have a meeting next week")
+print 
+print "test 5"
+schedule_meeting("Benedict, schedule a meeting for next Tuesday")
+print 
+print "test 6"
+schedule_meeting("Next Wednesday let's have a conference")
+print 
+print "test 7 "
+schedule_meeting("Can we have a weekly meeting on Thursdays?")
+print 
+print "test 8"
+schedule_meeting("Can you schedule me a meeting for tomorrow?")
+print 
+print "test 9"
+schedule_meeting("Schedule a meeting for tomorrow at 5 pm")
+print 
 
 
-schedule_meeting("Schedule a meeting for tomorrow at 11")
+#schedule_meeting("Schedule a meeting for tomorrow at 6 am")
 #schedule_meeting("can you schedule a meeting for tomorrow")
