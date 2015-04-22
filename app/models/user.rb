3d3ffa773b321f2bc7d5b87986761aa5fd1ceea2
Kevin Malta <kevin.malta@gmail.com>
2015-04-21 17:30:03 -0700

    fixes name truncation and name placement in room divs

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.name = auth.info.name
+      user.name = auth.info.name.split("@")[0]
       user.email = auth.info.email
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
