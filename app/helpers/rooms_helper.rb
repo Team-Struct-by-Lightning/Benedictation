c24dffbdad1c7bfd34844f7b54ccc5b7434bc0da
Kevin Malta <kevin.malta@gmail.com>
2015-04-30 18:49:27 -0700

    arbitrary wolfram strings pop up in center div

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -110,8 +110,11 @@ module RoomsHelper
 	def query_wolfram_alpha(json_hash)
 		coder = HTMLEntities.new
 		query_string = json_hash['query'].to_s
+		query_string = query_string.split(" " + "\u2018s" + " ").join("s ")
+		query_string = query_string.split(" 's ").join("s ")
+		puts "&&&&&&&&&&&&&&&&: " + query_string
 		app_id = WolframAPIKey["app_id"]
-		wolfram_url = URI.parse("http://api.wolframalpha.com/v2/query?appid=P3P4W5-LGWA2A3RU2&input=" + "moon" + "&format=image").to_s
+		wolfram_url = URI.parse("http://api.wolframalpha.com/v2/query?appid=P3P4W5-LGWA2A3RU2&input=" + query_string + "&format=image").to_s
 		puts "@@@@@@@@@@@@wolfram url: " + wolfram_url
 		doc = Nokogiri::XML(open(wolfram_url))
 		images = doc.xpath("//img")
