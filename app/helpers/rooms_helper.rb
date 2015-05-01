86a21d49db4a79138e72f7faf2867e09d1704a59
Evan Crook <evannc@gmail.com>
2015-04-30 20:17:24 -0700

    rails-side schedule suggestion first draft

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -263,4 +263,32 @@ module RoomsHelper
                         		:parameters => {'calendarId' => 'primary',
                         										'eventId' => id})
 	end
+
+	def get_available_times(starttime, endtime)
+		client = Google::APIClient.new
+		client.authorization.client_id = GoogleAPIKeys["app_id"]
+		client.authorization.client_secret = GoogleAPIKeys["secret"]
+		client.authorization.scope = "https://www.googleapis.com/auth/calendar"
+		client.authorization.refresh_token = current_user.refresh_token
+		client.authorization.access_token = current_user.oauth_token
+
+		if client.authorization.refresh_token && client.authorization.expired?
+		  client.authorization.fetch_access_token!
+		end
+
+		service = client.discovered_api('calendar', 'v3')
+
+		# This will list all busy times within the week of April 13 - 17
+		result = client.execute(:api_method => service.events.list,
+								:parameters => {'calendarId' => 'primary',
+												'timeMin'	 => starttime,
+												'timeMax'	 => endtime,
+												'orderBy' 	 => 'startTime',
+												'singleEvents' => true})
+		events = result.data.items
+		events.each do |e|
+			puts e.summary
+			puts e.start + " to " + e.end
+		end	
+	end
 end
