453fe898a81ab2c7ce999d523faeb824df359f2b
Kevin Malta <kevin.malta@gmail.com>
2015-05-09 02:35:35 -0700

    completely comments the rooms center renders code

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -13,6 +13,12 @@ include RoomsHelper
   	render nothing: true
   end
 
+
+  # Hey Britt, I haven't thought through this enough but will this
+  # hold for concurrency issues?  It seems like we only store one
+  # value in Redis here and it is possible if two people try to make
+  # the request at the same time, this could return one user's wolfram
+  # html to another user.  I could be mistaken here, could you let me know?
   def get_wolfram
     hash = {}
     hash['result'] = $redis.get("wolfram_html").to_s
