e412700c9f1be9ae79d73c17884013fc2ee3717f
Kevin Malta <kevin.malta@gmail.com>
2015-05-11 16:11:58 -0700

    fixes wolfram post to changed function

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -22,4 +22,8 @@ include RoomsHelper
     render :json => hash
   end
 
+
+  # do we need an additional function so that we always get wolfram due to requerying wolfram via
+  # the textbox? Or if wiki is better, display a warning and put a wiki link to ask the user if
+  # they want to switch?
 end
