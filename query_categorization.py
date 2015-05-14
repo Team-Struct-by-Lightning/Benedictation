ae44c05306964742eb105997ab77684cc643feca
Kevin Malta <kevin.malta@gmail.com>
2015-05-13 19:47:06 -0700

    adds code for training api_predictor without feature translation

--- query_categorization.py
+++ query_categorization.py
@@ -36,65 +36,156 @@ from nltk_brain import *
 #index 20: has more than 20 words
 
 
-
-features_array = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
-
 #training set of strings
 #it is a list of query, api_type
 
-training_set = [["Let's meet up on Friday at 3 p.m.", "calendar"],
-								["Can we have a meeting next week", "calendar"],
-								["Benedict, schedule a meeting for next Tuesday", "calendar"],
-								["Next Wednesday let's have a conference", "calendar"],
-								["Can you schedule me a meeting for tomorrow", "calendar"],
-								["Could you find a time that works for all of us", "calendar"],
-								["Can we have a weekly meeting on Thursdays", "calendar"],
-								["Would you be available after 2pm on Wednesday", "calendar"],
-								["Lets meet tomorrow at 12", "calendar"],
-								["Set up a meeting for tomorrow at 12", "calendar"],
-								["Let's meet later", "calendar"],
-								["Schedule a meeting tomorrow.", "calendar"],
-								["What is the phase of the moon", "wolfram"],
-								["Why are dogs", "google"],
-								["What is the rate of expansion of the universe", "wolfram"],
-								["When is my birthday", "google"],
-								["Why do dogs have feet", "google"],
-								["What are the biggest problems facing humanity", "google"],
-								["When can I see my children again", "google"],
-								["What are these spots on my genitals", "google"],
-								["Why do birds suddenly appear", "google"],
-								["Where is a good place to get Chinese food", "google"]]
+training_set = [("Let's meet up on Friday at 3 p.m.", "calendar"),
+								("Can we have a meeting next week", "calendar"),
+								("Benedict, schedule a meeting for next Tuesday", "calendar"),
+								("Next Wednesday let's have a conference", "calendar"),
+								("Can you schedule me a meeting for tomorrow", "calendar"),
+								("Could you find a time that works for all of us", "calendar"),
+								("Can we have a weekly meeting on Thursdays", "calendar"),
+								("Would you be available after 2pm on Wednesday", "calendar"),
+								("Lets meet tomorrow at 12", "calendar"),
+								("Set up a meeting for tomorrow at 12", "calendar"),
+								("Let's meet later", "calendar"),
+								("Schedule a meeting tomorrow.", "calendar"),
+								("What is the phase of the moon", "wolfram"),
+								("Why are dogs", "google"),
+								("What is the rate of expansion of the universe", "wolfram"),
+								("When is my birthday", "google"),
+								("Why do dogs have feet", "google"),
+								("What are the biggest problems facing humanity", "google"),
+								("When can I see my children again", "google"),
+								("What are these spots on my genitals", "google"),
+								("Why do birds suddenly appear", "google"),
+								("Where is a good place to get Chinese food", "google")]
 
 
-#api predicting bernoulli naive bayesian classifier
+def train_predictor(set_to_train_on, classes, predictor):
+	predictor.fit(set_to_train_on, classes)
 
-api_predictor = BernoulliNB()
 
-def train(set_to_train_on):
-	api_predictor.fit()
+def validate_predictor(set_to_validate_on, classes, predictor):
 
-#Convert training set to binary feature arrays
-training_feature_arrays = []
+	number_valid = 0
 
-def put_training_set_through_query(set_to_train_on):
+	for i in range (len(set_to_validate_on)):
 
-	for i in range 0 to len(set_to_train_on):
-		temp_array = query_to_array(set_to_train_on[i][0], set_to_train_on[i][1])
-		training_feature_arrays.append(temp_array)
+		if api_predictor.predict(set_to_validate_on[i]) == classes[i]:
+			number_valid += 1
 
-def query_to_array(query, api_type):
+	# gross ass code by Brown Clow
+	# num_valid = sum((api_predictor.predict(val) == j for val, j in zip(set_to_validate_on, classes)))
+	# returns the rate
+	return number_valid
 
-	temp_array = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+
+
+def change_query_string_to_int_array(set_to_train_on):
+
+	#Convert training set to binary feature arrays
+	training_feature_arrays = []
+
+	for i in range (len(set_to_train_on)):
+
+		temp_query_array = query_to_array(set_to_train_on[i][0])
+
+		# appends the integer features to the feature array
+		training_feature_arrays.append(temp_query_array)
+
+	return training_feature_arrays
+
+
+def change_api_type_array_to_int_array(class_array):
+
+	# this is used to hold the integer value of the api_type
+	training_class_array = []
+
+	for i in range (len(class_array)):
+
+		temp_class = change_api_type_to_int(class_array[i][1])
+
+		# changes class to the class array
+		training_class_array.append(temp_class)
+
+	return training_class_array
+
+
+def change_api_type_to_int(class_type):
+
+	# this is a switch statement on the api type
+
+	if class_type == "calendar":
+		return 1
+	elif class_type == "calendar_show":
+		return 2
+	elif class_type == "schedule_suggest":
+		return 3
+	elif class_type == "google_doc":
+		return 4
+	elif class_type == "wolfram":
+		return 5
+	elif class_type == "wikipedia":
+		return 6
+	else: # Defaults the google search if something messes up
+		return 7
+
+def percent_to_index(percentage, length_of_array):
+
+	return int(percentage*length_of_array)
+
+
+def query_to_array(query):
+
+	temp_array = np.array([0 for i in range(21)])
 
 	#call list of functions to populate the array
 	####
 
 	return temp_array
 
+
+def make_predictor(training_set, test_set_percentage):
+
+	assert(len(training_set) > 1)
+	# api predicting bernoulli naive bayesian classifier
+	api_predictor = BernoulliNB()
+
+	#random shuffles
+	training_set_shuffle = random.shuffle(training_set)
+
+	# convert training set to binary feature arrays
+	training_feature_arrays = change_query_string_to_int_array(training_set_shuffle)
+
+	# this is used to hold the integer value of the api_type
+	training_class_array = change_api_type_array_to_int_array(training_set_shuffle)
+
+	# returns the index
+	index = percent_to_index(test_set_percentage, len(training_set))
+
+	valid_num = 0
+
+	if index == len(training_feature_arrays):
+		train_predictor(training_feature_arrays, training_class_array, api_predictor)
+		valid_num = -1
+	else:
+		train_predictor(training_feature_arrays[:index + 1], training_class_array[:index + 1], api_predictor)
+		valid_num = validate_predictor(training_feature_arrays[index + 1:], training_class_array[index + 1:], api_predictor)
+		print "The number of valid predictions is ratio is: " +
+					str(valid_num) + "\nThe total number of predictions made is: " +
+					str(len(training_feature_arrays[index + 1:])) + "\nThe validation ratio is: " +
+					str(float(valid_num)/(len(training_feature_arrays[index + 1:])))
+
+	return api_predictor
+
+
+
+
 #EXAMPLE usage of BernoulliNB
 # X = np.random.randint(2, size=(6, 100))
 # Y = np.array([1, 2, 3, 4, 4, 5])
 # clf = BernoulliNB()
 # clf.fit(X, Y)
-# BernoulliNB(alpha=1.0, binarize=0.0, class_prior=None, fit_prior=True)
 # print(clf.predict(X[2]))
